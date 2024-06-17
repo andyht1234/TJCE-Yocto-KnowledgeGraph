6a13d6c11d621090ef22213ef18247e5989622ac
YiR2002 <81906788+YiR2002@users.noreply.github.com>
2024-06-17 22:39:37 +0800

    Update urls.py

--- PFC/PFC/urls.py
+++ PFC/PFC/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from PFC import views
-
+#urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.indexpage),
